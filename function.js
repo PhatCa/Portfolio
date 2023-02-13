2f35cfa21e8be24eb06415980b859729d0be4778
PhatCa <phatca@hawaii.edu>
2023-02-12 14:12:55 -1000

    finish portfolio

--- function.js
+++ function.js
@@ -20,3 +20,41 @@ function scrollToSection(sectionId){
 }
 
 
+// vertical nav bar in aboutme
+const experienceButton = document.querySelector('.navbar-skill-button-experience')
+const experienceInfo = document.querySelector('.experience')
+const educationButton = document.querySelector('.navbar-skill-button-education')
+const educationInfo = document.querySelector('.education')
+const skillButton = document.querySelector('.navbar-skill-button-skill')
+const skillInfo = document.querySelector('.skill')
+
+educationButton.addEventListener("click",()=>{
+    if(experienceInfo.classList.contains('show-education')){
+        experienceInfo.classList.remove('show-education')
+    }
+    if(skillInfo.classList.contains('show-education')){
+        skillInfo.classList.remove('show-education')
+    }
+    educationInfo.classList.toggle('show-education')
+})
+
+experienceButton.addEventListener("click",()=>{
+    if(educationInfo.classList.contains('show-education')){
+        educationInfo.classList.remove('show-education')
+    }
+    if(skillInfo.classList.contains('show-education')){
+        skillInfo.classList.remove('show-education')
+    }
+    experienceInfo.classList.toggle('show-education')
+})
+
+skillButton.addEventListener("click",()=>{
+    if(educationInfo.classList.contains('show-education')){
+        educationInfo.classList.remove('show-education')
+    }
+    if(experienceInfo.classList.contains('show-education')){
+        experienceInfo.classList.remove('show-education')
+    }
+
+    skillInfo.classList.toggle('show-education')
+})
